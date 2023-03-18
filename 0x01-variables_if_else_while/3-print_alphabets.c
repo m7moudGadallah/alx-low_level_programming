6c1307c563d34fb4bdd7ca696f9bf39377f2ab4d
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-18 03:33:11 -0700

    add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabet in lowercase & uppercase
+  * Return: (0) Scuess
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('A' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
