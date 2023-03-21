cdedaf92d5be4b5332fade2fa03b0b1074c4f6a4
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-21 14:35:14 -0700

    - Fix bug
    - Add _putchar.c that contains implementation of _putchar function
    - Modify 0-putchar.c by removing it's content and adding main functoin

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+  * main - print '_putchar' with using putchar
+  * Return: (0) Sucess
+  */
+int main(void)
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+
+	register int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i++]);
+	}
+
+	_putchar('\n');
+
+	return (0);
 }
