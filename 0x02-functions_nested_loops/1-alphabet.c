c00f25307e8bde8fb075bd22e781ec140e5f0cac
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-21 14:53:11 -0700

    Fix C styling in 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
+/**
+  * print_alphabet - print all lowercasee alphabet
+  */
 void print_alphabet(void)
 {
 	register int i = 0;
 
 	while (i < 26)
 	{
-		_putchar('a'+i);
+		_putchar('a' + i);
 		++i;
 	}
 
