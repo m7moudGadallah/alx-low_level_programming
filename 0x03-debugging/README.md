43cb06451d7f9d811de0764842b307c853899a18
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-23 12:50:59 -0700

    Add `0. Multiple mains` Task1
    - Add main.h file
    - Add positive_or_negative.c file
    - Add 0-main.c file
    - Enhance README file

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,2 +1,6 @@
 # 0x03. C - Debugging
 
+|Task|files|
+|----|-----|
+|0. Multiple mains|[main.h](./main.h)   [positive_or_negative.c](./positive_or_negative.c)  [0-main.c](./0-main.c)|
+
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * positive_or_negative - check number is positive or negative or zero
+  *
+  * @n: integer number we need to check it
+  */
+void positive_or_negative(int n)
+{
+	if (!n)
+	{
+		printf("%d is zero", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+}
