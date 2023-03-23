ee2b6bc9848936787ba0ce0944d029d90327b08a
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-23 15:09:28 -0700

    add Task1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _isupper - check if char is uppercase or not
+  *
+  * @c: char we need to check
+  *
+  * Return: (1) if c is uppercase, (0) otherwise
+  */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,5 @@
 # 0x04-more_functions_nested_loops
+
+- ***[main.h](./main.h)***
+- ***[0. isupper](./0-isupper.c)***
+
