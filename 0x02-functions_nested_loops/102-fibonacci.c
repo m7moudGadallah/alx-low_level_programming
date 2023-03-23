45c8c9a62057bdb338db57a4ef7a3a476524b43c
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-22 19:17:20 -0700

    Fix a bug in 100-print_times_table.c
    - Modify spaces with printf for multiplication res

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
-  * main - print first 100 fibonacci numbers
+  * main - print first 50 fibonacci numbers
   *
   * Return: (0) Scuess
   */
 int main(void)
 {
 	register int i;
-	int pp, p, curr;
+	long int pp, p, curr;
 
 	pp = 1;
 	p = 2;
 
 	printf("1");
 
-	for (i = 2; i < 100; i++)
+	for (i = 2; i < 50; i++)
 	{
 		curr = p + pp;
 		pp = p;
 		p = curr;
 
-		printf(", %d", curr);
+		printf(", %ld", curr);
 	}
 
 	printf("\n");
