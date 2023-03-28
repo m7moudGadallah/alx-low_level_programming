87d19af2a0ae019c3ed445fa0cbcad3e72bffdf9
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-28 01:07:26 -0700

    Fix bug in Task 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,9 @@ void puts_half(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	for (i = len / 2; i < len; i++)
+	for (i = (len / 2) + (len % 2); i < len; i++)
 	{
-		_putchar(str[len]);
+		_putchar(str[i]);
 	}
 
 	_putchar('\n');
