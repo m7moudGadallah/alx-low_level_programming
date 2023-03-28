5c6b6101da72bba3c9d68c8e3cf8c45695e63f62
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-27 18:48:26 -0700

    Add Task1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - reset int number to be 98
+  *
+  * @n : pointer to int
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+# 0x05-pointers_arrays_strings
+
+***[main.h](./main.h)***
+
+|Task|Files|
+|----|-----|
+|0. 98 Battery st.|[0-reset_to_98.c](./0-reset_to_98.c)|
+
