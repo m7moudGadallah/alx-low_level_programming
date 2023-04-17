eccadb845b19c7a8d7b1cc54ce0323f9dc635ac5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-04-17 16:39:13 -0700

    Add Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - print current file name
+  *
+  * Return: (0) Sucess
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -4,4 +4,5 @@
 |----|-----|
 |0. Object-like Macro|[0-object_like_macro.h](./0-object_like_macro.h)|
 |1. Pi|[1-pi.h](./1-pi.h)|
+|2. File name|[2-main.c](./2-main.c)|
 
